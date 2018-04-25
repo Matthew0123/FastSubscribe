356931b5a959dfc9079df9b9a710a7daf1402758
Matthew0123 <634629769@qq.com>
2018-04-25 17:26:49 +0800

    列表内容左右滑动，邮箱验证

--- fastsubscribe/pages/contentlist/contentlist.js
+++ fastsubscribe/pages/contentlist/contentlist.js
@@ -20,7 +20,6 @@ function queryNews(optiontabs,that)
         that.setData({
           tabs: optiontabs
         })
-        console.log(that.data.tabs);
       }
       else
       {
@@ -32,13 +31,19 @@ function queryNews(optiontabs,that)
     }
   }); 
 }
+var touchDot = 0;//触摸时的原点 
+var time = 0;// 时间记录，用于滑动时且时间小于1s则执行左右滑动 
+var interval = "";// 记录/清理 时间记录 
+var tmpFlag = true;// 判断左右华东超出菜单最大值时不再执行滑动事件
 
 Page({
   data: {
     activeIndex: 0,
     tabs: [],
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    tabLength:0,
+    nthMax:0
   },
   onLoad: function (options) {
     var that = this;
@@ -47,8 +52,8 @@ Page({
     wx.getSystemInfo({
       success: function (res) {
         that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length) + 10,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+          tabLength: (res.windowWidth / optiontabs.length),
+          nthMax: optiontabs.length
         });
       }
     });
@@ -68,5 +73,49 @@ Page({
     wx.navigateTo({
       url: '../webview/webview?link=' + encodeURIComponent(e.currentTarget.dataset.text)
     })
+  },
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+  // 触摸移动事件
+  touchMove: function (e) {
+    var touchMove = e.touches[0].pageX; 
+    // 向左滑动 
+    if(touchMove - touchDot <= -40 && time < 10)
+    { 
+      if (tmpFlag && this.data.activeIndex < this.data.nthMax)
+      { 
+        this.setData({
+          activeIndex: ++this.data.activeIndex > this.data.nthMax ? this.data.nthMax : this.data.activeIndex,
+          sliderOffset: this.data.sliderOffset + this.data.tabLength,
+        })
+        tmpFlag = false;
+        var optiontabs = this.data.tabs;
+        queryNews(optiontabs, this);
+      } 
+    } 
+    // 向右滑动 
+    if(touchMove - touchDot >= 40 && time < 10)
+    { 
+      if (tmpFlag && this.data.activeIndex > 0)
+      { 
+        this.setData({
+          activeIndex:--this.data.activeIndex < 0 ? 0 : this.data.activeIndex,
+          sliderOffset: this.data.sliderOffset - this.data.tabLength,
+        })
+        tmpFlag=false;
+        var optiontabs = this.data.tabs;
+        queryNews(optiontabs, this);
+      } 
+    } // touchDot = touchMove; //每移动一次把上一次的点作为原点（好像没啥用）     
+  },
+  touchEnd: function (e) {
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+    tmpFlag = true; // 回复滑动事件
   }
 });
